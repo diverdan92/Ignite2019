2c199b2216b1cdd0846cf98de8a6d3d1d7aee886
Daniel Jacobson <dajaco@microsoft.com>
2019-11-04 07:36:10 -0800

    Pre-demo prep

--- Ignite2019/MainWindow.xaml.cs
+++ Ignite2019/MainWindow.xaml.cs
@@ -25,7 +25,6 @@ namespace DotNetConf
 			InitializeComponent();
 			AppCenter.Start("a18d8658-8a46-4ce6-a63a-43fbb45facb5",
 				   typeof(Analytics), typeof(Crashes));
-			//foo;
 			_paper = new PaperTrail(this);
 			ProcessKey('0');
 			EraseDisplay = true;
@@ -323,8 +322,8 @@ namespace DotNetConf
 				_paper.AddResult("Error");
 				string equation = _paper.args;
 				Crashes.TrackError(e, new Dictionary<string, string>() { { "Equation: ", equation } });
-				//throw e;
-				MessageBox.Show(parent, "Operation cannot be perfomed", parent.Title);
+				throw e;
+				//MessageBox.Show(parent, "Operation cannot be perfomed", parent.Title);
 			}
 
 			return d;
